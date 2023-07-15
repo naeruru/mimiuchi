0181c1e3e22093adf30e6c22c33d6199c04ea54d
nae <nae@risu.dev>
2023-07-15 06:52:35 -0500

    clean up webhook.ts

--- src/helpers/webhook.ts
+++ src/helpers/webhook.ts
@@ -1,23 +1,14 @@
-const webhook = {
-    post : async function(url: string, body: any) {
-        console.log(body)
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'POST',
-                mode: 'no-cors',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Access-Control-Allow-Origin": '*',
-                },
-                body: JSON.stringify(body)
-            }).then(response => {
-                return resolve(response)
-            }).catch(error => {
-                reject(error)
-            })
-        })
-    }
+export async function post(url: string, body: any) {
+    return fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": '*',
+        },
+        body: JSON.stringify(body)
+    })
 }
 
-export default webhook
+export default { post }
