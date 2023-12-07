059d60c5daf1f1f69d9381fe1063f73533dc7506
JereMiO <jcotiniaux@gmail.com>
2023-12-07 19:02:39 +0100

    chore(*): add lint rules to keep order Vue SFC

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
 const antfu = require('@antfu/eslint-config').default
 
-module.exports = antfu()
+module.exports = antfu({
+  rules: {
+    'vue/block-order': ['error', {
+      order: [['template', 'script'], 'style'],
+    }],
+    'no-console': 'off',
+  },
+})
