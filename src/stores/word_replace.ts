6f07dde7bef1331ce95949da266d28cdd464698d
Saya <117558001+fuwako@users.noreply.github.com>
2024-03-02 20:51:30 -0500

    adjust regex to remove word boundaries

--- src/stores/word_replace.ts
+++ src/stores/word_replace.ts
@@ -23,12 +23,12 @@ export const useWordReplaceStore = defineStore('wordreplace', {
       // Replace words.
       // Keys are case-sensitive (e.g., "Hello" and "hello" are different replacement keys).
       // Longer keys have higher priority. (E.g., "Hello world" → "apple", "Hello" → "banana". The transcript "Hello world" will become "apple" instead of "banana world".) Keys are sorted when Word Replace is unmounted.
-      // Word boundaries prevent unexpected replacements (e.g, "script" → "HELLO" would cause "description" → "deHELLOion").
+      // Users are responsible for defining their own word boundaries. This design choice has more support for various languages and speech styles.
       const joined = Object.keys(this.word_replacements)
         .map(key => this.escapeRegExp(key))
         .join("|")
 
-      const replace_re = new RegExp(`(?<![\\w*])(${joined})(?![\\w*])`, "g")
+      const replace_re = new RegExp(joined, "g")
       
       return input.replace(replace_re, (matched) => {
         return this.word_replacements[matched]
