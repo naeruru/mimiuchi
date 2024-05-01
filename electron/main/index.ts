22bd94280c3e685885c659cb3170e0aebeb66e93
Saya <117558001+fuwako@users.noreply.github.com>
2024-05-01 19:50:31 -0400

    merge new code into window_config, use window_config

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -56,32 +56,23 @@ const window_config: any = {
   title: 'Main window',
   width: 1000,
   height: 700,
-  icon: join(process.env.PUBLIC, 'favicon.ico'),
+  icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
   frame: false,
   webPreferences: {
     preload,
     // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+    // nodeIntegration: true,
+
     // Consider using contextBridge.exposeInMainWorld
     // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+    // contextIsolation: false,
     nodeIntegration: true,
-    contextIsolation: true, // was false
+    contextIsolation: true,
   },
 }
 
 async function createWindow() {
-  win = new BrowserWindow({
-    title: 'Main window',
-    icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
-    webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // nodeIntegration: true,
-
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      // contextIsolation: false,
-    },
-  })
+  win = new BrowserWindow(window_config)
 
   if (VITE_DEV_SERVER_URL) { // #298
     win.loadURL(VITE_DEV_SERVER_URL)
