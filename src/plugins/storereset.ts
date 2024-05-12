10094ad2ba3580436b2e93d4c422263164ad450b
nae <nae@risu.dev>
2024-05-12 15:34:49 -0500

    remove unnecessary assign in resetStore()

--- src/plugins/storereset.ts
+++ src/plugins/storereset.ts
@@ -1,6 +1,6 @@
 export default function storeReset({ store }: any) {
   const initialState = JSON.parse(JSON.stringify(store.$state))
   store.$reset = () => store.$patch(($state: any) => {
-    $state = Object.assign($state, initialState)
+    Object.assign($state, initialState)
   })
 }
