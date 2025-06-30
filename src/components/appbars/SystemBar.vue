b7c3514f0e3910e2e560dda1f82ce41a7293d92a
naeruru <nae@risu.dev>
2025-06-29 21:55:45 -0500

    Better Mac support (#57)
    * add better mac support, fix issue with saving electron store
    
    * systembar formatting

--- src/components/appbars/SystemBar.vue
+++ src/components/appbars/SystemBar.vue
@@ -1,37 +1,43 @@
 <template>
   <v-system-bar height="35" class="systembar pr-0" color="background" window>
+
+    <div v-if="platform.includes('mac')" :style="{ 'min-width': '75px' }"></div>
+
     <v-icon class="me-2" icon="mdi-weather-night" />
 
     <span class="font-weight-medium">{{ APP_NAME }}</span>
+    
     <v-chip class="ml-2" size="x-small" label>
       {{ t('general.beta') }}
     </v-chip>
 
     <v-spacer />
 
-    <v-btn class="systembar-button" variant="text" height="35" size="x-small" @click="minimize">
-      <v-icon icon="mdi-minus" />
-    </v-btn>
+    <div v-if="!platform.includes('mac')">
+      <v-btn class="systembar-button" variant="text" height="35" size="x-small" @click="minimize">
+        <v-icon icon="mdi-minus" />
+      </v-btn>
 
-    <v-btn variant="text" class="systembar-button ms-2" height="35" size="x-small" @click="toggle_maximize">
-      <v-icon :icon=" maximized ? 'mdi-checkbox-multiple-blank-outline' : 'mdi-checkbox-blank-outline'" />
-    </v-btn>
+      <v-btn variant="text" class="systembar-button ms-2" height="35" size="x-small" @click="toggle_maximize">
+        <v-icon :icon=" maximized ? 'mdi-checkbox-multiple-blank-outline' : 'mdi-checkbox-blank-outline'" />
+      </v-btn>
 
-    <v-hover>
-      <template #default="{ isHovering, props }">
-        <v-btn
-          v-bind="props"
-          :color="isHovering ? 'red' : undefined"
-          :variant="isHovering ? `flat` : `text`"
-          class="systembar-button ms-2"
-          height="35"
-          size="x-small"
-          @click="close_app"
-        >
-          <v-icon icon="mdi-close" />
-        </v-btn>
-      </template>
-    </v-hover>
+      <v-hover>
+        <template #default="{ isHovering, props }">
+          <v-btn
+            v-bind="props"
+            :color="isHovering ? 'red' : undefined"
+            :variant="isHovering ? `flat` : `text`"
+            class="systembar-button ms-2"
+            height="35"
+            size="x-small"
+            @click="close_app"
+          >
+            <v-icon icon="mdi-close" />
+          </v-btn>
+        </template>
+      </v-hover>
+    </div>
   </v-system-bar>
 </template>
 
@@ -46,14 +52,7 @@ declare const window: any
 
 const APP_NAME = __APP_NAME__
 const maximized = ref(false)
-
-// onMounted(() => {
-//   if (is_electron()) {
-//     window.ipcRenderer.on('maximized_state', (event: any, data: any) => {
-//       maximized.value = event
-//     })
-//   }
-// })
+const platform = ref(window.navigator.platform.toLowerCase())
 
 function close_app() {
   if (is_electron())
